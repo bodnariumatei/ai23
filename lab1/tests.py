c4920da950d991d990ecaf27b4d759c796827d02
bodnariumatei <mateinicolae589@gmail.com>
2023-03-08 10:25:27 +0200

    lab1

--- lab1/tests.py
+++ lab1/tests.py
@@ -8,21 +8,21 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(get_last_word_lexicographically("ab aa ah as ar at azz aza"), "azz")
 
         self.assertEqual(compute_euclidian_distance(1,5,4,1), 5.0)
-        #self.assertEqual(compute_euclidian_distance(2, 3, 5, 7), 5.0)
+        self.assertEqual(compute_euclidian_distance(4, 1, 1, 5), 5.0)
 
         self.assertEqual(scalar_multiplication([1, 0, 2, 0,3], [1, 2, 0, 3, 1]), 4)
-        self.assertEqual(scalar_multiplication([1, 0, 2, 0,3], [1, 2, 0, 3, 1]), 4)
+        self.assertEqual(scalar_multiplication([1, 2, 0,3], [1, 2, 3, 1]), 8)
 
         self.assertEqual(get_unique_words("ana are ana are mere rosii ana"), ["mere", "rosii"])
-        self.assertEqual(get_unique_words("ana are ana are mere rosii ana"), ["mere", "rosii"])
+        self.assertEqual(get_unique_words("un soim pe un pisc cu un soim pe un pisc pix in plisc"), ["cu","pix", "in", "plisc"])
 
+        self.assertEqual(get_twin_number([1,3,4,2,3]), 3)
         self.assertEqual(get_twin_number([1,2,3,4,2]), 2)
-        self.assertEqual(get_twin_number([1,2,3,4,2]), 2)
 
-        self.assertEqual(get_number_in_majority([2,8,7,2,2,5,2,3,1,2,2]), 2)
-        self.assertEqual(get_number_in_majority([2,8,7,2,2,5,2,3,1,2,2]), 2)
+        self.assertEqual(get_number_in_majority([2, 8, 7, 1, 4, 5, 0, 3, 1, 2, 2]), "Nu exista nr majoritar")
+        self.assertEqual(get_number_in_majority([2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2]), 2)
 
-        self.assertEqual(get_kth_biggest_number([7, 4, 6, 3, 9, 1], 2), 7)
+        self.assertEqual(get_kth_biggest_number([7, 4, 6, 3, 9, 1], 5), 3)
         self.assertEqual(get_kth_biggest_number([7, 4, 6, 3, 9, 1], 2), 7)
 
 
